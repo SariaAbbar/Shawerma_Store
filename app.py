aa013a19362876d66c1c7d01a23d40af4dae56a3
Saria <sariaabbar986@gmail.com>
2023-10-25 14:36:38 +0300

    fix the code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# importing the Flask and user modules.
 from flask import Flask, render_template, request, redirect, url_for, session
 from user import Order
 import json
@@ -7,14 +8,16 @@ app = Flask(__name__)
 app.secret_key = "7d733430c3aedc51ab4fab8987c4e7e8df19f1c35d6e589a5220b58d5f39489cbfdcxghtfd"
 
 
-# rendering the home page
+#  The home page is rendered using the function called home().
+#  This function returns an HTML file that will be displayed when visiting the homepage.
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("index.html")
 
 
-# rendering the buying page
+#  The buying page is also rendered using another function called buying().
 @app.route("/buying")
 def buying():
     return render_template("buying.html")
@@ -26,7 +29,7 @@ def done():
     return render_template("done.html")
 
 
-# function that take the data and save it then redirecting to the done page
+# function that takes the data and saves it then redirecting to the done page.
 @app.route("/checkout", methods=["POST"])
 def checkout():
     order = Order(request.form)
@@ -35,6 +38,7 @@ def checkout():
 
 # route for admin
 # he should add the correct username and password 
+#  The code checks to see if the user is logged in, and if not it redirects them back to the login page.
 @app.route("/admin", methods=["GET", "POST"])
 def admin():
     error = None
